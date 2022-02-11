8819d5c7749681087fdb74d42ac462600d7dd0f3
mansourlotfi <mansourlotfi@gmail.com>
2022-02-11 21:09:43 +0330

    remove extra nav Items

--- src/_pages/layout/navbar.tsx
+++ src/_pages/layout/navbar.tsx
@@ -34,138 +34,15 @@ function Navbar() {
 
         <div id="navigation">
           <ul className="navigation-menu nav-left">
-            <li className="has-submenu parent-menu-item">
-              <a href="javascript:void(0)">خانه</a>
+            <li>
+              <Link to="/">خانه</Link>
               <span className="menu-arrow"></span>
-              <ul className="submenu">
-                <li>
-                  <a href="index.html" className="sub-menu-item">
-                    خانه یک
-                  </a>
-                </li>
-                <li>
-                  <a href="index-two.html" className="sub-menu-item">
-                    خانه دو
-                  </a>
-                </li>
-                <li>
-                  <a href="index-three.html" className="sub-menu-item">
-                    خانه سه
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <li className="has-submenu parent-parent-menu-item">
-              <a href="javascript:void(0)">پزشکان</a>
-              <span className="menu-arrow"></span>
-              <ul className="submenu">
-                <li className="has-submenu parent-menu-item">
-                  <a href="javascript:void(0)" className="menu-item">
-                    داشبرد
-                  </a>
-                  <span className="submenu-arrow"></span>
-                  <ul className="submenu">
-                    <li>
-                      <a href="doctor-dashboard.html" className="sub-menu-item">
-                        داشبرد
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="doctor-appointment.html"
-                        className="sub-menu-item"
-                      >
-                        نوبت دهی
-                      </a>
-                    </li>
-                    <li>
-                      <a href="patient-list.html" className="sub-menu-item">
-                        بیماران
-                      </a>
-                    </li>
-                    <li>
-                      <a href="doctor-schedule.html" className="sub-menu-item">
-                        زمانبندی برنامه
-                      </a>
-                    </li>
-                    <li>
-                      <a href="invoices.html" className="sub-menu-item">
-                        فاکتورها
-                      </a>
-                    </li>
-                    <li>
-                      <a href="patient-review.html" className="sub-menu-item">
-                        نظرات
-                      </a>
-                    </li>
-                    <li>
-                      <a href="doctor-messages.html" className="sub-menu-item">
-                        پیام ها
-                      </a>
-                    </li>
-                    <li>
-                      <a href="doctor-profile.html" className="sub-menu-item">
-                        پروفایل
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="doctor-profile-setting.html"
-                        className="sub-menu-item"
-                      >
-                        تنظیمات پروفایل
-                      </a>
-                    </li>
-                    <li>
-                      <a href="doctor-chat.html" className="sub-menu-item">
-                        چت
-                      </a>
-                    </li>
-                    <li>
-                      <a href="login.html" className="sub-menu-item">
-                        ورود
-                      </a>
-                    </li>
-                    <li>
-                      <a href="signup.html" className="sub-menu-item">
-                        ثبت‌نام
-                      </a>
-                    </li>
-                    <li>
-                      <a href="forgot-password.html" className="sub-menu-item">
-                        فراموشی رمزعبور
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="doctor-team-one.html" className="sub-menu-item">
-                    پزشکان یک
-                  </a>
-                </li>
-                <li>
-                  <a href="doctor-team-two.html" className="sub-menu-item">
-                    پزشکان دو
-                  </a>
-                </li>
-                <li>
-                  <a href="doctor-team-three.html" className="sub-menu-item">
-                    پزشکان سه
-                  </a>
-                </li>
-              </ul>
             </li>
 
             <li className="has-submenu parent-menu-item">
               <a href="javascript:void(0)">بیماران</a>
               <span className="menu-arrow"></span>
               <ul className="submenu">
-                <li>
-                  <a href="patient-dashboard.html" className="sub-menu-item">
-                    داشبرد
-                  </a>
-                </li>
                 <li>
                   <Link to="/patientProfile" className="sub-menu-item">
                     پروفایل
@@ -175,51 +52,6 @@ function Navbar() {
                   <Link to="/bookingAppointment" className="sub-menu-item">
                     رزرو نوبت
                   </Link>
-                </li>
-                <li>
-                  <a href="patient-invoice.html" className="sub-menu-item">
-                    فاکتور
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <li className="has-submenu parent-menu-item">
-              <a href="javascript:void(0)">داروخانه</a>
-              <span className="menu-arrow"></span>
-              <ul className="submenu">
-                <li>
-                  <a href="pharmacy.html" className="sub-menu-item">
-                    داروخانه
-                  </a>
-                </li>
-                <li>
-                  <a href="pharmacy-shop.html" className="sub-menu-item">
-                    فروشگاه
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="pharmacy-product-detail.html"
-                    className="sub-menu-item"
-                  >
-                    جزییات دارو
-                  </a>
-                </li>
-                <li>
-                  <a href="pharmacy-shop-cart.html" className="sub-menu-item">
-                    سبد خرید
-                  </a>
-                </li>
-                <li>
-                  <a href="pharmacy-checkout.html" className="sub-menu-item">
-                    ادامه خرید
-                  </a>
-                </li>
-                <li>
-                  <a href="pharmacy-account.html" className="sub-menu-item">
-                    اکانت
-                  </a>
                 </li>
               </ul>
             </li>
@@ -233,11 +65,7 @@ function Navbar() {
                     درباره ما
                   </Link>
                 </li>
-                <li>
-                  <a href="departments.html" className="sub-menu-item">
-                    دپارتمان ها
-                  </a>
-                </li>
+
                 <li>
                   <Link to="/faqs" className="sub-menu-item">
                     سوالات متدوال
@@ -254,43 +82,21 @@ function Navbar() {
                         بلاگ
                       </Link>
                     </li>
-                    <li>
-                      <a href="blog-detail.html" className="sub-menu-item">
-                        جزییات بلاگ
-                      </a>
-                    </li>
                   </ul>
                 </li>
-                <li>
-                  <a href="terms.html" className="sub-menu-item">
-                    مقررات و سیاست‌ها
-                  </a>
-                </li>
+
                 <li>
                   <Link to="/privacy" className="sub-menu-item">
                     حفظ حریم شخصی
                   </Link>
                 </li>
-                <li>
-                  <a href="error.html" className="sub-menu-item">
-                    404 !
-                  </a>
-                </li>
+
                 <li>
                   <Link to="/contactUs" className="sub-menu-item">
                     ارتباط با ما
                   </Link>
                 </li>
               </ul>
-            </li>
-            <li>
-              <a
-                href="../admin/index.html"
-                className="sub-menu-item"
-                target="_blank"
-              >
-                ادمین
-              </a>
             </li>
           </ul>
         </div>
