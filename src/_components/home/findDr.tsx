d786a92963edfc5164bbf2cc2bcbf509b268a8f0
mansourlotfi <mansourlotfi@gmail.com>
2022-02-11 10:15:47 +0330

    remove extra section

---
+++ src/_components/home/findDr.tsx
@@ -0,0 +1,31 @@
+function FindDr() {
+  return (
+    <section
+      className="bg-half-170 pb-0 d-table w-100"
+      style={{
+        backgroundImage: "url('../assets/images/bg/bg-lines-one.png')",
+      }}
+    >
+      <div className="container">
+        <div className="row mt-5 mt-sm-0 align-items-center">
+          <div className="col-md-6">
+            <div className="heading-title">
+              <h4 className="heading mb-3"> بهترین پزشک را پیدا کنید </h4>
+              <p className="para-desc text-muted mb-0">
+                {" "}
+                اگر به عضو خانواده خود برای دریافت کمک فوری ، درمان اضطراری یا
+                یک مشاوره ساده نیاز دارید ، پزشکان ما را ببینید.{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-6 mt-4 pt-2 mt-sm-0 pt-sm-0">
+            <img src="../assets/images/hero.png" className="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FindDr;
