18768adce9e39360e59012c9daa6afd8d6978bf4
mansourlotfi <mansourlotfi@gmail.com>
2022-02-18 11:13:44 +0330

    implement reserve ui based on api need

--- src/_components/bookingAppointment/formHandler.tsx
+++ src/_components/bookingAppointment/formHandler.tsx
@@ -5,117 +5,74 @@ function FormHandler() {
   const { drAvailableDates } = useAppSelector((state) => state.DrDates);
 
   return (
-    <form>
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="mb-3">
-            <label className="form-label">
-              نام بیمار<span className="text-danger">*</span>
-            </label>
-            <input
-              name="name"
-              id="name2"
-              type="text"
-              className="form-control"
-              placeholder="نام بیمار:"
-            />
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="mb-3">
-            <label className="form-label">
-              تاریخ<span className="text-danger">*</span>
-            </label>
-            <select className="form-control doctor-name select2input">
-              {drAvailableDates &&
-                drAvailableDates.map((item) => (
-                  <option value={item.id}>{item.date}</option>
-                ))}
-            </select>
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="mb-3">
-            <label className="form-label">
-              ایمیل شما <span className="text-danger">*</span>
-            </label>
-            <input
-              name="email"
-              id="email2"
-              type="email"
-              className="form-control"
-              placeholder="ایمیل شما"
-            />
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="mb-3">
-            <label className="form-label">
-              شماره شما <span className="text-danger">*</span>
-            </label>
-            <input
-              name="phone"
-              id="phone2"
-              type="tel"
-              className="form-control"
-              placeholder="شماره شما :"
-            />
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="mb-3">
-            <label className="form-label"> تاریخ: </label>
-            <input
-              name="date"
-              type="text"
-              className="flatpickr flatpickr-input form-control"
-              id="checkin-date"
-              placeholder="1400-12-06"
-            />
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="mb-3">
-            <label className="form-label">زمان : </label>
-            <input
-              name="time"
-              type="text"
-              className="form-control timepicker"
-              id="input-time"
-              placeholder="03:30 PM"
-            />
-          </div>
-        </div>
-
-        <div className="col-lg-12">
-          <div className="mb-3">
-            <label className="form-label">
-              نظرات<span className="text-danger">*</span>
-            </label>
-            <textarea
-              name="comments"
-              id="comments2"
-              rows={4}
-              className="form-control"
-              placeholder="پیام شما:"
-            ></textarea>
-          </div>
-        </div>
-
-        <div className="col-lg-12">
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
-              رزرو نوبت
-            </button>
-          </div>
+    <div className="row">
+      <div className="col-md-6">
+        <div className="mb-3">
+          <label className="form-label">
+            شماره همراه <span className="text-danger">*</span>
+          </label>
+          <input
+            name="phone"
+            id="phone"
+            type="text"
+            className="form-control"
+            placeholder="شماره همراه"
+          />
         </div>
       </div>
-    </form>
+
+      <div className="col-md-6">
+        <div className="mb-3">
+          <label className="form-label">
+            تاریخ<span className="text-danger">*</span>
+          </label>
+          <select className="form-control doctor-name select2input">
+            {drAvailableDates &&
+              drAvailableDates.map((item) => (
+                <option value={item.id}>{item.date}</option>
+              ))}
+          </select>
+        </div>
+      </div>
+
+      <div className="col-md-6">
+        <div className="mb-3">
+          <label className="form-label">
+            زمان <span className="text-danger">*</span>
+          </label>
+          <input
+            name="time"
+            type="text"
+            className="form-control timepicker"
+            id="input-time"
+            placeholder="03:30 PM"
+          />
+        </div>
+      </div>
+
+      <div className="col-lg-12">
+        <div className="mb-3">
+          <label className="form-label">
+            توضیحات<span className="text-danger">*</span>
+          </label>
+          <textarea
+            name="description"
+            id="description"
+            rows={4}
+            className="form-control"
+            placeholder="توضیحات"
+          ></textarea>
+        </div>
+      </div>
+
+      <div className="col-lg-12">
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            رزرو نوبت
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
